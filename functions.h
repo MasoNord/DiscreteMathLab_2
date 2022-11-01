ad2cd9e3c27f038fadb261f41a3dded4c2c1a7a4
MasoNord <112730325+MasoNord@users.noreply.github.com>
2022-11-01 18:55:08 +0200

    Add files via upload

--- functions.h
+++ functions.h
@@ -1,347 +1,243 @@
-#pragma once
 #include <iostream>
-#include <algorithm>
 #include <vector>
-#include <math.h>
+#include <algorithm>
 #include <Windows.h>
 
-// In 1,3,4 tasks I made 2 "copy", when A was bigger then B and reverse
-// This was made by me for predicting exceptions, when our programm calls an unexistable element
+// ------------------ Second task ----------------- 
 
-// ---------------- Was created for output a values of arrays ---------------- //
+bool function(int x, int y);
 
-void Out_Multiply_Vector(std::vector<std::vector<int>> out) 
+// ------------------------------------------------
+
+// ------------------ Second task -----------------
+
+bool Injective(int x, int y);
+bool Surjective(int x, int y);
+bool Bijective(int x, int y);
+
+// ------------------------------------------------
+
+void OutputVector(std::vector<std::vector<int>> array);
+void OutputSimpleVector(std::vector<int> array);
+
+void First(std::vector<int> A, std::vector<int> B)
 {
-	std::cout << "\n{\n";
-	for (int i = 0; i < out.size(); i++)
-	{
-		std::cout << "( ";
-		for (int j = 0; j < out[i].size(); j++)
-			std::cout << out[i][j] << " ";
-		std::cout << ")\n";
-	}
-
-	std::cout << "}\n";
-} 
-void Out_Vector(std::vector<int> a)
-{
-	std::cout << "{ ";
-	for(int o = 0; o < a.size(); o++)
-	{
-		std::cout << a[o] << " ";
-	}
-	std::cout << "}\n";
-}
-// --------------------------------------------------------------------------- //
-
-// -------------------- This function use for second task -------------------- //
-
-void Subset(std::vector <int>& mainV, std::vector<std::vector<int>> &res,
-	std::vector <int>& subset, int index)
-{
-	res.push_back(subset);
-	
-	for (int i = index; i < mainV.size(); i++)
-	{
-		subset.push_back(mainV[i]);
-
-		Subset(mainV, res, subset, i + 1);
-
-		subset.pop_back();
-	}
-}
-
-// --------------------------------------------------------------------------- //
-
-// -------------------- This function use for fourth task -------------------- //
-
-std::vector<int> Union(std::vector<int> A, std::vector<int> B, int size_A, int size_B,
-	std::vector <int> &result)
-{
-	int i = 0;
-	
-	if (size_A > size_B)
-	{
-		for (i = 0; i < size_B; i++)
-		{
-			if (std::count(B.begin(), B.end(), A[i]))
-				result.push_back(A[i]);
-			else
-			{
-				result.push_back(A[i]);
-				result.push_back(B[i]);
-			}
-		}
-		for (; i < size_A; i++)
-			result.push_back(A[i]);
-	}
-	else if (size_A < size_B)
-	{
-		for (i = 0; i < size_A; i++)
-		{
-			if (std::count(A.begin(), A.end(), B[i]))
-				result.push_back(A[i]);
-			else
-			{
-				result.push_back(A[i]);
-				result.push_back(B[i]);
-			}
-		}
-		for (i; i < size_B; i++)
-			result.push_back(B[i]);
-	}
-	
-	else
-		result = A;
-	
-	return result;
-}
-std::vector<int> Intersection(std::vector<int> A, std::vector<int> B, int size_A, int size_B,
-	std::vector <int> &result)
-{
-	if (size_A > size_B)
-	{
-		for (int i = 0; i < size_B; i++)
-			if (std::count(B.begin(), B.end(), A[i]))
-				result.push_back(A[i]);
-	}
-
-	else if (size_A < size_B)
-	{
-		for (int i = 0; i < size_A; i++)
-			if (std::count(A.begin(), A.end(), B[i]))
-				result.push_back(B[i]);
-	}
-
-	else
-		result = A;
-
-	return result;
-}
-
-std::vector<int>Difference(std::vector<int> A, std::vector<int> B, int size_A,
-	std::vector <int> &result)
-{
-	for (int i = 0; i < size_A; i++)
-		if (!std::count(B.begin(), B.end(), A[i]))
-				result.push_back(A[i]);
-
-	return result;
-}
-
-std::vector<int> Disunction(std::vector<int> A, std::vector<int> B, int size_A, int size_B,
-	std::vector <int> &result)
-{	
-
-	if (size_A > size_B)
-	{
-		for (int i = 0; i < size_B; i++)
-		{
-			if (!std::count(A.begin(), A.end(), B[i]))
-			{
-				result.push_back(B[i]);
-			}
-			if (!std::count(B.begin(), B.end(), A[i]))
-			{
-				result.push_back(A[i]);
-			}
-		}
-		for (int i = 0; i < size_A; i++)
-			if ((!std::count(result.begin(), result.end(), A[i])) && (!std::count(B.begin(), B.end(), A[i])))
-				result.push_back(A[i]);
-	}
-	else
-	{
-		for (int i = 0; i < size_A; i++)
-		{
-			if (!std::count(A.begin(), A.end(), B[i]))
-			{
-				result.push_back(B[i]);
-			}
-			if (!std::count(B.begin(), B.end(), A[i]))
-			{
-				result.push_back(A[i]);
-			}
-		}
-		for (int i = 0; i < size_B; i++)
-			if ((!std::count(result.begin(), result.end(), B[i])) && (!std::count(A.begin(), A.end(), B[i])))
-				result.push_back(B[i]);
-	}
-
-	return result;
-}
-
-std::vector<int> Complement(std::vector<int> A, std::vector<int> U, int size_A, int size_U,
-	std::vector<int> &result)
-{
-	int i = 0;
-	
-	for (i; i < size_A; i++)
-		if (!std::count(A.begin(), A.end(), U[i]))
-			result.push_back(U[i]);
-	
-	for (i; i < size_U; i++)
-		result.push_back(U[i]);
-
-	return result;
-}
-
-
-// --------------------------------------------------------------------------- //
-
-// First task
-void First(std::vector<int> a, std::vector<int> b)
-{
-	bool pass = true;
-	int size_a = a.size();
-	int size_b = b.size();
-
-	std::sort(a.begin(), a.end());
-	std::sort(b.begin(), b.end());
-
-	if (size_a == size_b)
-	{
-		for (int i = 0; i < size_a; i++)
-		{
-			if (std::count(b.begin(), b.end(), a[i]) == false)
-			{
-				std::cout << "They are not equivalent!\n";
-				pass = false;
-				break;
-			}
-		}
-		if (pass == true)
-			std::cout << "They are equivalent!\n";
-	}
-	else
-	{
-		if (size_a > size_b)
-		{
-			for (int i = 0; i < size_b; i++)
-			{
-				if (!std::count(a.begin(), a.end(), b[i]))
-				{
-					pass = false;
-					std::cout << "B is not subset of A!\n";
-					break;
-				}
-			}
-			if (pass != false)
-				std::cout << "B is subset of A!\n";
-		}
-		else
-		{
-			
-			for (int i = 0; i < size_a; i++)
-			{
-				if (!std::count(b.begin(), b.end(), a[i]))
-				{
-					pass = false;
-					std::cout << "A is not subset of B! \n";
-					break;
-				}
-			}
-			if (pass != false)
-				std::cout << "A is subset of B!\n";
-		}
-	}
-} 
-
-// Second task
-void Second(std::vector<int> mainV)
-{
-	std::vector<int> subset;
-
-	std::vector<std::vector<int>> res;
-
-	int index = 0;
-
-	Subset(mainV, res, subset, index);
-	
-	Out_Multiply_Vector(res);
-}
-
-// Third task
-void Third(std::vector<int> A, std::vector<int> B)
-{
-	std::vector<std::vector<int>> res;
-	std::vector <std::vector<int>> res1;
-	std::vector<int> subset;
-	
-	for (int i = 0; i < A.size(); i++)
-	{
-		for (int j = 0; j < B.size(); j++)
-		{
-			subset.push_back(A[i]);
-			subset.push_back(B[j]);
-
-			res.push_back(subset);
-			subset.pop_back();
-			subset.pop_back();
-		}
-	}
-
-	std::cout << "A * B = ";
-
-	Out_Multiply_Vector(res);
-	std::cout << std::endl;
-	
-
-	for (int i = 0; i < B.size(); i++)
-	{
-		for (int j = 0; j < A.size(); j++)
-		{
-			subset.push_back(B[i]);
-			subset.push_back(A[j]);
-
-			res1.push_back(subset);
-			subset.pop_back();
-			subset.pop_back();
-		}
-	}
-
-	std::cout << "\nB * A = ";
-
-	Out_Multiply_Vector(res1);
-}
-
-// Fourth
-void Fourth(std::vector<int> A, std::vector<int> B, std::vector<int> U)
-{
+	std::vector<std::vector<int>> result;
+	int SurCount = 0;
 	int size_A = A.size();
 	int size_B = B.size();
-	int size_U = U.size();
-	int i;
+	int count = 0;
+	std::vector<int> temp;
+	
+	sort(A.begin(), A.end());
+	sort(B.begin(), B.end());
 
-	std::vector<int> UnionRes, IntersectionRes, DifferenceRes, DisunctionRes, ComplementRes;
+	for (int i = 0; i < size_A; i++)
+	{
+		for (int j = 0; j < size_B; j++)
+		{
+			if((function(A[i], B[j]) == true))
+			{
+				temp.push_back(A[i]);
+				temp.push_back(B[j]);
+					
+				result.push_back(temp);
+				count++;
+				temp.erase(temp.begin(), temp.end());
+			}
+		}
+	}
+	
+	OutputVector(result);
+	
+	if (size_A < size_B)
+	{
+		if (count == A.size())
+			std::cout << "f: A -> B true\n";
+		else
+			std::cout << "f: A -> B false\n";
+	}
+	else if(size_A > size_B)
+	{
+		for (int i = 0, j = 1; j < result.size(); j++)
+		{
+			if (result[i][1] == result[j][1])
+				SurCount++;
+		}
+
+		if (count == A.size() && SurCount >= 1)
+			std::cout << "\nf: A -> B true\n";
+		else
+			std::cout << "f: A -> B false\n";
+	}
+
+}
+
+void Second(std::vector<int> A, std::vector<int> B)
+{
+	std::vector<std::vector<int>> result;
+	int size_A = A.size();
+	int size_B = B.size();
+	int count = 0;
+	int SurCount = 0;
+	short answer = 0;
+	std::vector<int> temp;
 
 	sort(A.begin(), A.end());
 	sort(B.begin(), B.end());
-	sort(U.begin(), U.end());
 
-	Union(A, B, size_A, size_B, UnionRes);
-	Intersection(A, B, size_A, size_B, IntersectionRes);
-	Difference(A, B, size_A, DifferenceRes);
-	Disunction(A, B, size_A, size_B, DisunctionRes);
-	Complement(A, U, size_A, size_U, ComplementRes);
-	
-	std::cout << "\nUnion of A and B = ";
-	Out_Vector(UnionRes);
-	std::cout << std::endl;
+	std::cout << "Enter function (1, 2, 3): ";
+	std::cin >> answer;
+	switch (answer)
+	{
+	case 1: // Injective
+		std::cout << "f(x) = 5x\n";
+		
+		for (int i = 0; i < size_A; i++)
+		{
+			for (int j = 0; j < size_B; j++)
+			{
+				if (Injective(A[i], B[j]) == true)
+				{
+					temp.push_back(A[i]);
+					temp.push_back(B[j]);
 
-	std::cout << "Intersection of A and B = ";
-	Out_Vector(IntersectionRes);
-	std::cout << std::endl;
+					result.push_back(temp);
+					count++;
+					temp.erase(temp.begin(), temp.end());
+				}
+			}
+		}
+		if (count == A.size())
+			std::cout << "f: A -> B is injective\n";
+		else
+			std::cout << "f: A -> B is not injective\n";
+		
+		OutputVector(result);
+		
+		result.clear();
+		count = 0;
+		break;
+	case 2: // Surjective
+		std::cout << "f(x) =  x * x - 1\n";
+		for (int i = 0; i < size_A; i++)
+		{
+			for (int j = 0; j < size_B; j++)
+			{
+				if (Surjective(A[i], B[j]) == true)
+				{
+					temp.push_back(A[i]);
+					temp.push_back(B[j]);
 
-	std::cout << "Difference of A and B = ";
-	Out_Vector(DifferenceRes);
-	std::cout << std::endl;
+					count++;
+					result.push_back(temp);
+					temp.erase(temp.begin(), temp.end());
+				}
+			}
+		}
 
-	std::cout << "Disunction of A and B = ";
-	Out_Vector(DisunctionRes);
-	std::cout << std::endl;
+		for (int i = 0, j = 1; j < result.size(); j++)
+		{
+			if (result[i][1] == result[j][1])
+				SurCount++;
+		}
 
-	std::cout << "Complement of A = ";
-	Out_Vector(ComplementRes);
+		if (count == A.size() && SurCount >= 1)
+			std::cout << "\nf: A -> B is Surjective\n";
+		else
+			std::cout << "f: A -> B is not Surjective\n";
 
+		OutputVector(result);
+
+		count = 0;
+		result.clear();
+		break;
+	case 3: // Bijective
+		std::cout << "f(x) = 2x - 3\n";
+
+		for (int i = 0; i < size_A; i++)
+		{
+			for (int j = 0; j < size_B; j++)
+			{
+				if (Bijective(A[i], B[j]) == true)
+				{
+					temp.push_back(A[i]);
+					temp.push_back(B[j]);
+
+					result.push_back(temp);
+					count++;
+					temp.erase(temp.begin(), temp.end());
+				}
+			}
+		}
+
+		if (count == A.size() && count == B.size())
+			std::cout << "f: A -> B is Bijective\n";
+		else
+			std::cout << "f: A -> B is not Bijective\n";
+
+		OutputVector(result);
+
+		count = 0;
+		result.clear();
+		break;
+	default:
+
+		break;
+	}
+
+}
+// Start:
+// First task
+bool function(int x, int y)
+{
+	// f(x) = x * x;
+
+	if ((x * x) == y )
+		return true;
+}
+// The end
+
+// Start:
+// Second task
+bool Injective(int x, int y)
+{
+	// f(x) = 5x
+	if ((5 * x) == y)
+		return true;
+}
+
+bool Surjective(int x, int y)
+{
+	// f(x) =  x * x + 1
+	if (((x * x) + 1) == y)
+		return true;
+}
+
+bool Bijective(int x, int y)
+{
+	// f(x) = 2x - 3
+	if ((2 * x - 3) == y)
+		return true;
+}
+// The end
+void OutputVector(std::vector<std::vector<int>> array)
+{
+	std::cout << "{\n";
+	for (int i = 0; i < array.size(); i++)
+	{
+		std::cout << "( ";
+		for (int j = 0; j < array[i].size(); j++)
+			std::cout << array[i][j] << " ";
+		std::cout << ")\n";
+	}
+	std::cout << "}\n";
+}
+
+void OutputSimpleVector(std::vector<int> array)
+{
+	std::cout << "{ ";
+	for (int i = 0; i < array.size(); i++)
+		std::cout << array[i] << " ";
+	std::cout << "}\n";
 }
--- main.cpp
+++ main.cpp
@@ -1,124 +1,102 @@
-// For different realisation of program we'll use different methods of including
-// If you use tags you need to rewrite into this
-// #include "functions.h" 
-#include "C:\Users\trotf\University\Discrete math\header\functions.h"
+#include "C:/Users/trotf/University/DiscreteMath/Lab_2/header/functions.h"
 
-void GetValues(std::vector<int>& array, std::string info);
-void ReWrite(std::vector<int>& ReArray, std::string NewInfo);
+void GetValue(std::vector<int>& array);
+
+void ReWrite(std::vector<int>& array, std::string info);
 
 int main()
 {
-	std::vector<int> A; // first array
-	std::vector<int> B; // second array
-	std::vector<int> U; // universal set
+	system("color a");
+
+	std::vector<int> B;
+	std::vector<int> A;
+	short answer = -1;
+
 	
-	int answer = 1;
-
-	std::cout << "Enter -0 for ending your input\n";
-
-	GetValues(A, "Enter numbers for A array: ");
-	GetValues(B, "Enter numbers for B array: ");
-	GetValues(U, "Enter numbers for U array: ");
-
-
 	while (answer != 0)
 	{
-		std::cout << "\n------------------------------------------------\n";
+		std::cout << "\t\tFirst task\n\t\tf(x) = x * x\n";
+		std::cout << "\t\tSecond task\n\t\tf(x) = 5x (Injective)\n\t\tf(x) = x * x + 1 (Surjective)\n\t\tf(x) = 2x - 3 (Bijective)\n";
+		
+		std::cout << "---------------------------------------------\n";
+		
+		std::cout << "1) Insert elemts in A and B array \n2) First task\n3) Second taks\n4) Rewrite A and B arrays"
+			<< "\n0) Exit";
 
-		std::cout << "0 - exit from the programm\n1 - first task\n"
-			<< "2 - second task\n3 - third task\n4 - fourth task\n"
-			<< "5 - rewrite A\n6 - rewrite B\n7 - rewrite U";
-
-		std::cout << "\n------------------------------------------------\n";
+		std::cout << "\n---------------------------------------------";
+		
 		std::cout << "\nYour choice: ";
-
 		std::cin >> answer;
 
 		switch (answer)
 		{
-		case 0:
-			break;
 		case 1:
-			std::cout << "A = ";
-			Out_Vector(A);
-			std::cout << "B = ";
-			Out_Vector(B);
+			std::cout << "Enter value for A array: ";
+			GetValue(A);
 
-			First(A, B);
+			std::cout << "Enter value for B array: ";
+			GetValue(B);
+
 			system("pause");
 			system("cls");
 			break;
 		case 2:
-			std::cout << "A = ";
-			Out_Vector(A);
-
-			Second(A);
+			std::cout << "Array A: ";
+			OutputSimpleVector(A);
+			
+			std::cout << "Array B: ";
+			OutputSimpleVector(B);
+			
+			First(A, B);
 			system("pause");
 			system("cls");
 			break;
 		case 3:
-			std::cout << "A = ";
-			Out_Vector(A);
-			std::cout << "B = ";
-			Out_Vector(B);
-			
-			Third(A, B);
+			std::cout << "Array A: ";
+			OutputSimpleVector(A);
+
+			std::cout << "Array B: ";
+			OutputSimpleVector(B);
+
+			Second(A, B);
 			system("pause");
 			system("cls");
 			break;
 		case 4:
-			std::cout << "A = ";
-			Out_Vector(A);
-			std::cout << "B = ";
-			Out_Vector(B);
-			std::cout << "U = ";
-			Out_Vector(U);
-
-			Fourth(A, B, U);
+			ReWrite(A, "Insert new elemnts for A array: ");
+			ReWrite(B, "Insert new elemnets for B array: ");
+			
 			system("pause");
 			system("cls");
 			break;
-		case 5:
-			ReWrite(A, "Enter new numbers for A: ");
-			break;
-		case 6:
-			ReWrite(B, "Enter new numbers for B: ");
-			break;
-		case 7:
-			ReWrite(U, "Enter new numbers for U: ");
+		case 0:
 			break;
 		default:
-			std::cerr << "Oops! You've entered incorrect number, try again\n";
-			system("pause");
-			system("cls");
+			std::cerr << "Incorrect value!\n";
 			break;
 		}
 	}
-
 	system("pause");
-
-	return 0;
 }
 
-void GetValues(std::vector<int>& array, std::string info)
+void ReWrite(std::vector<int>& array, std::string info)
+{
+	array.clear();
+	std::cout << info;
+	GetValue(array);
+}
+
+void GetValue(std::vector<int>& array)
 {
 	int c = 1;
 
-	std::cout << info << " ";
-
-	while (std::cin && c != '@')
+	while (std::cin && c != -0)
 	{
 		std::cin >> c;
+
 		if (c != -0)
+		{
 			array.push_back(c);
-		else
-			break;
+		}
 	}
-}
-
-void ReWrite(std::vector<int>& ReArray, std::string NewInfo)
-{
-	ReArray.clear();
-
-	GetValues(ReArray, NewInfo);
 }
